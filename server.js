99dec9913f47f7e03a2419c78019237507979fe1
jzarzeczny <jkzarzeczny@gmail.com>
2021-08-23 19:01:16 +0200

    deployment prep

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config({ path: "./config.env" });
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
-
+app.use(express.static(path.join(__dirname, "client", "build")));
 const dbo = require("./db/conn");
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port, () => {
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
