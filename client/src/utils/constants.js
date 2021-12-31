4eb6062e3f7fb0e62724237a237f9ce46fe76d9e
jzarzeczny <jkzarzeczny@gmail.com>
2021-12-31 20:11:58 +0100

    changing the host

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,7 +2,7 @@ let API = "";
 if (process.env.NODE_ENV === "development") {
   API = "http://localhost:5001";
 } else if (process.env.NODE_ENV === "production") {
-  API = "https://ax--cpt.herokuapp.com";
+  API = "http://axcpt.toadres.pl/";
 }
 
 export const API_HOST = API;
