5273c077aedb946098b87027a7a4838808850837
jzarzeczny <jkzarzeczny@gmail.com>
2022-01-01 19:39:48 +0100

    polishing the app

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,7 +2,7 @@ let API = "";
 if (process.env.NODE_ENV === "development") {
   API = "http://localhost:5001";
 } else if (process.env.NODE_ENV === "production") {
-  API = "http://localhost:5001";
+  API = "http://axcpt.toadres.pl/api";
 }
 
 export const API_HOST = API;
