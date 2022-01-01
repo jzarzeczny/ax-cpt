103e4f6384f50c51ddbcd635b5112416ea432bc3
jzarzeczny <jkzarzeczny@gmail.com>
2022-01-01 20:19:06 +0100

    another polishing

--- client/src/utils/sendData.js
+++ client/src/utils/sendData.js
@@ -3,7 +3,7 @@ import { API_HOST } from "./constants";
 
 const sendResults = async (nickname, path, data) => {
   data.forEach((e) => (e.nickname = nickname));
-  axios
+  await axios
     .post(API_HOST + "/" + path + nickname.nickname, data)
     .catch((e) => console.log(e));
 };
